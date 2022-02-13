d51fcec47e549a4470357df56606b5ad58760759
Mahesh C. Regmi <0xmahadev@gmail.com>
2022-02-13 23:04:33 +0545

    add env variable

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000/";
+
+export const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
 import axios, { AxiosInstance } from "axios";
 
 class APIService {
